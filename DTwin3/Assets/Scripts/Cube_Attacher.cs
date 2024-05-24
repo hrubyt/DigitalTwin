102d90915ed5c5552664877e98158c9eded98e36
hrubyt <thruby@proton.me>
2024-05-24 16:06:11 +0200

    cube_attacher update

--- DTwin3/Assets/Scripts/Cube_Attacher.cs
+++ DTwin3/Assets/Scripts/Cube_Attacher.cs
@@ -1,76 +1,3 @@
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Cube_Attacher : MonoBehaviour {
-
-    // Reference to cube
-    public GameObject cube; 
-    public GameObject cncDisc; 
-    public GameObject robotGripper;
-
-    public Cube_Spawner cubeSpawner;
-
-    // Attachment variables
-    [SerializeField] private bool attach_Gripper = false;
-    [SerializeField] private bool attach_CNC = false;
-    [SerializeField] private bool dettach = false;
-
-
-    /*
-    void Start() {
-        // Get reference to cube
-        cube = cubeSpawner.spawnedCube;
-    }
-    */
-/*
-void Update() {
-
-    // pokud kolize gripper kostka
-
-    // kolize cnc kostka
-
-
-
-        if (cubeSpawner.isSpawned) {
-            // Get reference to cube
-            if (cube != cubeSpawner.spawnedCube) {
-            cube = cubeSpawner.spawnedCube;
-            }           
-            if (dettach) {
-                DetachCube();
-            }
-            if (attach_CNC) {
-                DetachCube();
-                AttachCube(cncDisc);
-            } 
-            else if (attach_Gripper) {
-                DetachCube();
-                AttachCube(robotGripper);
-            } 
-                     
-        }
-               
-    }
-
-    // Method to attach the cube to the parent object
-    void AttachCube(GameObject parentObject) {
-        if (cube != null) {
-            // Attach the cube as a child of the parent object
-            cube.transform.parent = parentObject.transform;
-        }
-    }
-
-    // Method to detach the cube from the parent object
-    void DetachCube() {
-    if (cube != null) {
-        cube.transform.parent = null; // Detach the cube from the parent object
-    }
-}
-}
-
-*/
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -96,12 +23,13 @@ public class Cube_Attacher : MonoBehaviour {
     void Update() {
 
         if (cubeSpawner.isSpawned) {
-            // Získání odkazu na kostku
+            // get refference to cube
             if (cube != cubeSpawner.spawnedCube) {
                 cube = cubeSpawner.spawnedCube;
             }
             if (dettach) {
                 DetachCube();
+                dettach = false;
             }
             if (attach_CNC) {
                 DetachCube();
@@ -142,10 +70,10 @@ public class Cube_Attacher : MonoBehaviour {
     private void OnCollisionExit(Collision collision) {
         if (collision.gameObject == gripperFinger) {
             attach_Gripper = false;
-            dettach = true;
+            dettach = true;          
         } else if (collision.gameObject == workholder) {
             attach_CNC = false;
-            dettach = true;
+            dettach = true;        
         }
     }
 
